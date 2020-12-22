1b22242f5837641176b0766d570812d75f288af2
André <andre.kleinschmidt@digitalcareerinstitute.org>
2020-12-22 22:28:40 +0100

    Änderungen Burgermenu

--- src/hamburger.js
+++ src/hamburger.js
@@ -4,7 +4,7 @@ function hamburger_click(){
     let ham_i=document.querySelector('#hami')
 
     //ham_click.classList.add('clickon')
-
+/*
     if(ham_click.classList.contains('clickon')){
         ham_click.classList.add('clickoff')
         ham_click.classList.remove('clickon')
@@ -13,4 +13,14 @@ function hamburger_click(){
         ham_click.classList.remove('clickoff')
       
     }
+    */
+   ham_click.classList.toggle('clickon');
+    if (ham_click.classList.contains('clickon')) {
+        ham_i.classList.remove("fa-bars");
+        ham_i.classList.add("fa-times");
+    } else {
+        ham_i.classList.remove("fa-times");
+        ham_i.classList.add("fa-bars");   
+    }
+
 }
