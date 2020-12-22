744f84db7e3a73c97a8669743a8430bb07644745
ElhamHashemi <elhamhashemi56@yahoo.com>
2020-12-22 19:11:39 +0100

    hamburger

---
+++ src/hamburger.js
@@ -0,0 +1,13 @@
+
+function hamburger_click(){
+    let ham_click=document.querySelector('#ham')
+    //ham_click.classList.add('clickon')
+
+    if(ham_click.classList.contains('clickon')){
+        ham_click.classList.add('clickoff')
+        ham_click.classList.remove('clickon')
+    }else{
+        ham_click.classList.add('clickon')
+        ham_click.classList.remove('clickoff')
+    }
+}
